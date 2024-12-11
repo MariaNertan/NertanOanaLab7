957a991f0fc0df4f80468c533368de995b557ae8
Maria Nertan <marianertan@yahoo.com>
2024-12-11 19:24:14 +0200

    Finalizat tot + sarcina laborator

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -96,6 +96,12 @@ namespace NertanOanaLab7.Data
             } 
         }
 
+        public Task<int> DeleteShopAsync(Shop shop)
+        {
+            return _database.DeleteAsync(shop);
+        }
+
+
 
         public Task<List<Product>> GetListProductsAsync(int shoplistid) 
         { 
--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -1,31 +1,69 @@
-using Microsoft.Maui.Devices.Sensors;
 using NertanOanaLab7.Models;
 using Plugin.LocalNotification;
 namespace NertanOanaLab7;
 
 public partial class ShopPage : ContentPage
 {
-	public ShopPage()
-	{
-		InitializeComponent();
-	}
-    async void OnSaveButtonClicked(object sender, EventArgs e) 
-	{ 
-		var shop = (Shop)BindingContext; 
-		await App.Database.SaveShopAsync(shop); 
-		await Navigation.PopAsync(); 
-	}
+    public ShopPage()
+    {
+        InitializeComponent();
+    }
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext;
+        await App.Database.SaveShopAsync(shop);
+        await Navigation.PopAsync();
+    }
 
     async void OnShowMapButtonClicked(object sender, EventArgs e)
     {
-        var shop = (Shop)BindingContext; 
-		var address = shop.Adress; 
+        var shop = (Shop)BindingContext;
+        var address = shop.Adress;
 
-		var options = new MapLaunchOptions { Name = "Magazinul meu preferat" };
+        var options = new MapLaunchOptions { Name = "Magazinul meu preferat" };
 
-        var shoplocation= new Location(46.7492379, 23.5745597);//pentru Windows Machine */
+        var shoplocation = new Location(46.7492379, 23.5745597);//pentru Windows Machine */
 
 
-            await Map.OpenAsync(shoplocation, options);
+        var myLocation = await Geolocation.GetLocationAsync();       /*   var myLocation = new Location(46.7731796289, 23.6213886738); //pentru Windows Machine */
+        var distance = myLocation.CalculateDistance(shoplocation, DistanceUnits.Kilometers);
+        if (distance < 5)
+        {
+            var request = new NotificationRequest
+            {
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }
+
+        await Map.OpenAsync(shoplocation, options);
     }
+
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext;
+
+        if (shop == null)
+        {
+            await DisplayAlert("Eroare", "Magazinul nu a fost g?sit.", "OK");
+            return;
+        }
+
+        var confirm = await DisplayAlert("Confirmare", $"Sigur dore?ti s? ?tergi magazinul '{shop.ShopName}'?", "Da", "Nu");
+        if (confirm)
+        {
+            await App.Database.DeleteShopAsync(shop);
+            await DisplayAlert("Succes", "Magazinul a fost ?ters.", "OK");
+            await Navigation.PopAsync();
+        }
+    }
+
+
+
+
 }
